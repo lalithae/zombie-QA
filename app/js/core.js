20b1d3ee83b5e1ea89f9af01c669be70e24713b2
lalithae <lalitha@genesiis.com>
2015-09-19 13:07:24 +0530

    implemented fb login. note: server folder is excluded from deployment.

--- app/js/core.js
+++ app/js/core.js
@@ -1,19 +1,27 @@
+
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  
+Template.login.events({
+    'click #facebook-login': function(event) {
+        Meteor.loginWithFacebook({}, function(err){
+            if (err) {
+                throw new Meteor.Error("Facebook login failed");
+            }
+        });
+    },
+ 
+    'click #logout': function(event) {
+        Meteor.logout(function(err){
+            if (err) {
+                throw new Meteor.Error("Logout failed");
+            }
+        })
     }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+});
 }
 
 if (Meteor.isServer) {
---
+++ app/server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+ 
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '1498900753754443',
+    secret: '0419b8c3e421b0f466315660691fd84b'
+});
