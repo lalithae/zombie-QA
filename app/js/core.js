c9ebb7c0b449ecd7d653192fc842bea553a0109e
lalithae <lalitha@genesiis.com>
2015-09-19 16:39:43 +0530

    implemented zombie signup and sign in.

--- app/js/core.js
+++ app/js/core.js
@@ -4,6 +4,48 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
+
+
+  Template.body.events({
+	'submit #zombieSignup': function(event){
+		event.preventDefault();
+		var email = event.target.signUpEmail.value;
+		var passOne = event.target.passOne.value;
+		var passTwo = event.target.passTwo.value;
+		
+		if(passOne != passTwo){
+			$('#signup-msg').text("Passwords do not match");
+			return false;
+		}
+		
+		Accounts.createUser({
+			email: email,
+			password: passOne
+		}, function(err){		
+			if(err != null)
+				$('#signup-msg').text(err.reason);
+			else
+				$('#signup').modal('hide');
+		});
+	}, 
+	
+	'submit #zombieSignin': function(event){
+		event.preventDefault();
+		var email = event.target.signinEmail.value;
+		var passOne = event.target.signinPass.value;
+		
+		var user = {"email":email};
+		Meteor.loginWithPassword(user, passOne, function(err){
+			if(err != null)
+				$('#signin-msg').text(err.reason);
+			else
+				$('#signin').modal('hide');
+		});
+	}
+	
+  });
+  
+
   
 Template.login.events({
     'click #facebook-login': function(event) {
